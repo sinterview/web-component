e9ec344233d2a81432efe7734f1ad761d5566a89
Sam <sinterview@zippymail.info>
2015-09-30 18:36:29 +0530

    Add Preview Image Component
    The component supports drag and drop functionality. If the selected file
    is an image file it will preview the file.

--- src/client/js/app.main.js
+++ src/client/js/app.main.js
@@ -1,6 +1,9 @@
 "use strict";
 
-(function(global) {
+(function ModalDialogIIFE(global) {
+    var webComponentAPI = global.webComponentAPI || {};
+
+
     var buildModal = function buildModalFn() {
         var content, contentHolder,
         docFragment = document.createDocumentFragment();
@@ -39,7 +42,9 @@
         }
 
         docFragment.appendChild(this.modal);
-        document.body.appendChild(docFragment);
+        document.getElementById(this.options.modalDialogParentId)
+                .appendChild(docFragment);
+        //document.body.appendChild(docFragment);
     };
 
     var registerEvents = function registerEventsFn() {
@@ -55,10 +60,11 @@
     var ModalDialogBox = {
         init: function initFn(configObj) {
             var defaultConfig = {
-                className: "fade-and-drop",
+                modalDialogParentId: "modal-dialog",
+                className: "simple-modal",
                 closeButton: true,
                 content: "Hello-Modal",
-                minWidth: 280,
+                minWidth: 300,
                 overlay: true
             };
 
@@ -97,15 +103,143 @@
                     .replace(" modal-open", "");
             this.overlay.className = this.overlay.className
                     .replace(" overlay-open", "");
+
+            var modalHolder = document.getElementById(
+                    this.options.modalDialogParentId);
+            modalHolder.innerHTML = "";
+
             this.modalOpen = false;
         }
     };
 
 
-    global.modalDialogBox = (function setupModalDialog() {
+    webComponentAPI
+            .createModalDialogBox = function createModalDialogBox(configObj) {
         var modalDialogBox = Object.create(ModalDialogBox);
-        modalDialogBox.init();
+        modalDialogBox.init(configObj);
 
         return modalDialogBox;
-    })();
+    };
+
+    global.webComponentAPI = webComponentAPI;
+})(window);
+
+
+(function ImagePreviewIIFE(global) {
+    var webComponentAPI = global.webComponentAPI || {};
+
+
+    var ImagePreviewComponent = {
+        init: function initFn(configObj) {
+            if (!window.File || !window.FileList || !window.FileReader) {
+                throw new Error("File Drag and Drop not Supported"
+                        + " in this browser.");
+            }
+
+            var defaultConfig = {
+                fileDragDropId: "filedragdrop",
+                imageHolderId: "preview-image"
+            };
+
+            if (configObj && (typeof configObj === "object")) {
+                for (var property in configObj) {
+                    if (configObj.hasOwnProperty(property)) {
+                        defaultConfig[property] = configObj[property];
+                    }
+                }
+            }
+
+            this.options = defaultConfig;
+            this.configureDragDropElement();
+        },
+
+        configureDragDropElement: function configureDragDropElementFn() {
+            var dragDropElement = document.getElementById(
+                    this.options.fileDragDropId);
+
+            dragDropElement.addEventListener("dragover",
+                    function dragOverventHandler(event) {
+                        this.fileDragHandler(event);
+                    }.bind(this),
+                    false);
+
+            dragDropElement.addEventListener("dragleave",
+                    function dragLeaveEventHandler(event) {
+                        this.fileDragHandler(event);
+                    }.bind(this),
+                    false);
+
+            dragDropElement.addEventListener("drop",
+                    function dropEventHander(event) {
+                        this.fileSelectHandler(event);
+                    }.bind(this),
+                    false);
+            dragDropElement.style.display = "block";
+        },
+
+        fileDragHandler: function fileDragHandlerFn(event) {
+            event.stopPropagation();
+            event.preventDefault();
+            event.target.className = "";
+
+            if (event.type === "dragover") {
+                event.target.className = "hover";
+            }
+        },
+
+        fileSelectHandler: function fileSelectHandlerFn(event) {
+            this.fileDragHandler(event);
+
+            var uploadedFiles = event.target.files
+                    || event.dataTransfer.files;
+
+            for (var filesIndex = 0;
+                    filesIndex < uploadedFiles.length;
+                    filesIndex++) {
+                this.parseFiles(uploadedFiles[filesIndex]);
+            }
+        },
+
+        parseFiles: function parseFilesFn(file) {
+            var imageType = /^image\//;
+
+            if (imageType.test(file.type)) {
+                this.renderImageFile(file);
+                return;
+            }
+
+            console.log("Only Image Files are previewed.");
+        },
+
+        renderImageFile: function renderImageFileFn(fileObj) {
+            if (fileObj) {
+                var reader = new FileReader();
+
+                reader.onload = function readerOnLoadFn(event) {
+                    var imageHolder = document.createElement("div");
+                    imageHolder.className = "image-previewer";
+                    var imageContent = document.createElement("img");
+                    imageContent.setAttribute("alt", "uploaded image");
+                    imageContent.setAttribute("src", event.target.result);
+
+                    imageHolder.appendChild(imageContent);
+                    document.getElementById(this.options.imageHolderId)
+                            .appendChild(imageHolder);
+                }.bind(this);
+
+                reader.readAsDataURL(fileObj);
+            }
+        }
+    };
+
+
+    webComponentAPI.createImagePreviewComponent
+            = function createImagePreviewComponentFn(configObj) {
+        var imagePreviewComponent = Object.create(ImagePreviewComponent);
+        imagePreviewComponent.init(configObj);
+
+        return imagePreviewComponent;
+    };
+
+    global.webComponentAPI = webComponentAPI;
 })(window);
